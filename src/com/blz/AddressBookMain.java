47c47d74f513496ed21cb61e962543d3b3bdf069
TejShind <tej.gholap25@gmail.com>
2022-03-06 11:27:18 +0530

    [Tejaswini]ADDED:Feature for Edit existing contact details

--- src/com/blz/AddressBookMain.java
+++ src/com/blz/AddressBookMain.java
@@ -8,13 +8,33 @@ public class AddressBookMain {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Address Book Program");
         AddressBook addressBook = new AddressBook();
-        System.out.println("Enter number of contacts you want to add");
-        int contactsCount = scanner.nextInt();
-        for (int i = 0; i < contactsCount; i++) {
-            addressBook.addContacts();
-        }
-        for (int i = 0; i < contactsCount; i++) {
-            System.out.println(AddressBook.contacts.get(i));
+
+        System.out.println("1.Add Contact");
+        System.out.println("2.Edit Contact");
+        System.out.println("0.Exit");
+        int option = scanner.nextInt();
+        while (option != 0) {
+            switch (option) {
+                case 1 -> {
+                    System.out.println("Enter number of contacts you want to add");
+                    int contactsCount = scanner.nextInt();
+                    for (int i = 0; i < contactsCount; i++) {
+                        addressBook.addContacts();
+                    }
+                    for (int i = 0; i < contactsCount; i++) {
+                        System.out.println(AddressBook.contacts.get(i));
+                    }
+                }
+                case 2 -> {
+                    System.out.println("Enter the first name you want to edit");
+                    String check = scanner.next();
+                    addressBook.editContact(check);
+                }
+            }
+            System.out.println("1.Add Contact");
+            System.out.println("2.Edit Contact");
+            System.out.println("0.Exit");
+            option = scanner.nextInt();
         }
     }
 }
